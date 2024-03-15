bb935f2f13c99b45ed16711f9c884c01fef6445f
Pav20097569 <20097569@mail.wit.ie>
2024-03-15 20:25:21 +0000

    Added Polish as an extra language and changed colour theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("org.slf4j:slf4j-simple:1.7.36")
+    implementation ("io.github.microutils:kotlin-logging:2.1.23")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/ie/setu/mobileassignment/utils/Logger.kt
@@ -0,0 +1,8 @@
+package ie.setu.mobileassignment.utils
+
+import mu.KotlinLogging
+
+val log = KotlinLogging.logger {}
+
+class Logger {
+}
