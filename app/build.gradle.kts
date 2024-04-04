f71e8674a27d9c1804168cd471e45b3551945168
Pav20097569 <20097569@mail.wit.ie>
2024-04-04 22:14:25 +0100

    added code for nav bar but not fully working yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.ajalt:timberkt:1.5.1")
     implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.google.android.material:material:1.11.0")
+
 
 
 }
